85b45bbd31687ebd2ea3eb9ecac98cad13115a21
CLOATRE-Erwan <73162784+CLOATRE-Erwan@users.noreply.github.com>
2020-11-27 11:59:36 +0100

    Add files via upload

---
+++ source/python/joueurs.py
@@ -0,0 +1,19 @@
+
+class joueurs:
+
+    def __init__(self, id, nom, score, couleur):
+        self.id = id
+        self.nom = nom
+        self.score = score
+        self.couleur = couleur
+
+    @classmethod
+    def infos(cls, nb_joueurs):
+        lst = []
+        for i in range(nb_joueurs):
+            nouveau = joueurs(id, input(f"pseudo joueur n°{i+1}: "), 0, input("Choix de la couleur : "))
+            lst.append(nouveau)
+        return lst
+
+    # def score(self, rep):
+    #     self.score = self.score + rep

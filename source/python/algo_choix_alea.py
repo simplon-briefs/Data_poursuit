c8dc268c13d56f54043c79978c228be386a29c35
BaptisteLEBERRE <baptiste.le-berre@isen-ouest.yncrea.fr>
2020-11-27 16:07:48 +0100

    update requettes et algo

--- source/python/algo_choix_alea.py
+++ source/python/algo_choix_alea.py
@@ -6,14 +6,14 @@ class Question:
         self.id_question = id_question
         self.question = question
         self.reponse_true = reponse_true
-        self.reponse_fake = [reponse_fake]
+        self.reponse_fake = reponse_fake
 
-        self.reponse_all = reponse_fake[:]
+        self.reponse_all = list(reponse_fake)
         self.reponse_all.append(reponse_true)
  
     def display(self):
         print("id_question = ",self.id_question)
-        print("reponse = ", self.question)
+        print("question = ", self.question)
         print("reponse_true = ",self.reponse_true)
         print("reponse_fake = ", self.reponse_fake)
         print("reponse_all = ",self.reponse_all)
--- source/python/requettes.py
+++ source/python/requettes.py
@@ -1,4 +1,5 @@
 import mysql.connector as mysql
+from algo_choix_alea import Question
 
 class Connection:
     def __init__(self):
@@ -9,40 +10,31 @@ class Connection:
             database='trivial'
             )
         self.curseur = self.lien.cursor()
-cnx = Connection()
 
-def retour_db(colonne, table):
-    cnx.curseur.execute(f"SELECT {colonne} FROM {table}")
-    infos = list(cnx.curseur.fetchall())
-    return infos
+    def retour_db(self, colonne, table):
+        self.curseur.execute(f"SELECT {colonne} FROM {table}")
+        infos = list(self.curseur.fetchall())
+        return infos
 
-def retour_db_con(colonne, table, condition):
-    cnx.curseur.execute(f"SELECT {colonne} FROM {table} WHERE {condition}")
-    infos = list(cnx.curseur.fetchall())
-    return infos
+    def retour_db_con(self, colonne, table, condition):
+        self.curseur.execute(f"SELECT {colonne} FROM {table} WHERE {condition}")
+        infos = list(self.curseur.fetchall())
+        return infos
 
-def creer_dico(liste_cles, liste_valeurs):
-    dico = {}
-    for i in range(len(liste_cles)):
-        dico[str(*liste_cles[i])] = str(liste_valeurs[i])
-    return dico
+    def infos_questions(self):
+        liste_finale = list()
+        liste_questions = self.retour_db("libelle_question","questions")
+        for numero in range(3):
+            question = liste_questions[numero]
+            id_question = liste_questions.index(question) + 1
+            reponse_v = (self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 1")))[0]
+            reponse_f = self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 2"))
+
+            liste_finale.append(Question(id_question, question, reponse_v, reponse_f))
+        return liste_finale
 
 
-liste_questions = retour_db("libelle_question","questions")
-liste_reponses = retour_db("libelle_reponse", "reponses")
+liste_finale = Connection().infos_questions()
 
-dico_qr = creer_dico(liste_questions, liste_reponses)
-
-#for i in dico_qr:
-    #print(f"Question : {i}\nRéponse 1 : {dico_qr[i]}\nRéponse 2 : Réponse fausse")
-    #reponse_joueur = int(input("1 ou 2"))
-    #if reponse_joueur == 1:
-        #print("Correct !")
-
-question = liste_questions[int(input("n° question ?")) - 1]
-print(*question)
-id_question = liste_questions.index(question) + 1
-reponse_v = (retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 1")))[0]
-reponse_f = retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 2"))
-print(*reponse_v)
-print(reponse_f)
+for instance in liste_finale:
+    print(instance.display())

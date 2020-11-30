e626dd9962d3602c68f0a857e0c182fcdf1d14d7
CLOATRE-Erwan <erwan.cloatre@isen-ouest.yncrea.fr>
2020-11-30 14:22:18 +0100

    main update

--- source/python/main.py
+++ source/python/main.py
@@ -7,15 +7,15 @@ def get_random_question(liste):
     random.shuffle(liste)
     return liste 
 
-def verif_rep(rep_utilisateur, vrai_reponse, lst):
+# def verif_rep(rep_utilisateur, vrai_reponse, lst):
 
-    if rep_utilisateur == vrai_reponse:
-        lst[0].score = lst[0].score + 1
-        print(f"Bravo! Tu peux rejouer, Vos points : {lst[0].score}")
-        return True
-    else:
-        print(f"Mauvaise réponse, votre score est : {lst[0].score}")
-        return False
+#     if rep_utilisateur == vrai_reponse:
+#         lst[0].score = lst[0].score + 1
+#         print(f"Bravo! Tu peux rejouer, Vos points : {lst[0].score}")
+#         return True
+#     else:
+#         print(f"Mauvaise réponse, votre score est : {lst[0].score}")
+#         return False
 
 def main():
     cnx = Connection()
@@ -34,7 +34,7 @@ def main():
         while tmp :             
             print(liste_random[0].question)
             print(liste_random[0].reponse_all)
-            print(liste_random[0].reponse_true)
+            #print(liste_random[0].reponse_true)
             saisie_choix = input("entrez votre choix : ")
             #verif_rep(saisie_choix, question.reponse_true, lst_joueurs)
             i += 1

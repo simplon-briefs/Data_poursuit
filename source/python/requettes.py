7e3dcbf5c6b5cd28d4a14382e79e27a5d7055a15
BaptisteLEBERRE <73174593+BaptisteLEBERRE@users.noreply.github.com>
2020-12-01 12:30:56 +0100

    Add files via upload

--- source/python/requettes.py
+++ source/python/requettes.py
@@ -1,19 +1,15 @@
 import mysql.connector as mysql
 from question import Question
 
-class Connection:
+class Connexion:
     def __init__(self):
         self.lien = mysql.connect(
             host='localhost',
-            port = '8081',
             password='root',
             user='root',
             database='trivial'
             )
         self.curseur = self.lien.cursor()
-    
-
-
 
     def retour_db(self, colonne, table):
         self.curseur.execute(f"SELECT {colonne} FROM {table}")
@@ -25,24 +21,24 @@ class Connection:
         infos = list(self.curseur.fetchall())
         return infos
 
-    
-    def get_questions_reponse(self):
+
+    def infos_questions(self):
         liste_finale = list()
+        liste_theme = list()
         liste_questions = self.retour_db("libelle_question","questions")
-        range_reponse_true = self.retour_db_con("libelle_reponse", "reponses", "valeur_reponse = 1")
-        for numero in range(len(range_reponse_true)):
-            question = liste_questions[numero]
-            id_question = liste_questions.index(question) + 1
-            reponse_v = (self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 1")))
-            reponse_f = self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 0"))
+        for numero in range(3):
+            question_tuple = liste_questions[numero]
+            question = str(*question_tuple)
+            id_question = liste_questions.index(question_tuple) + 1
+            reponse_v = str(*(self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 1")))[0])
+            reponse_f = self.retour_db_con("libelle_reponse", "reponses", str(f"id_question = {id_question} AND valeur_reponse = 2"))
 
             liste_finale.append(Question(id_question, question, reponse_v, reponse_f))
-        return liste_finale
-    def display(self):
-        for i in self.liste_finale:
-            i.display()
-            print("------------")
+            liste_theme.append(*self.retour_db_con("difficulte_question", "questions", f"id_question = {id_question}")[0])
+        return liste_finale, liste_theme
 
 
+liste_finale, liste_theme = Connexion().infos_questions()
 
-#print(Connection().get_questions_reponse())
+for instance in liste_finale:
+    print(instance.display())
---
+++ source/python/theme_dicts.py
@@ -0,0 +1,17 @@
+import requettes
+from requettes import liste_finale, liste_theme
+
+liste_titres = ["Big Data", "Thème 2", "Thème 3", "Thème 4", "Thème 5"]
+
+def creer_dico_theme(index_theme):
+    liste_dico = []
+    dico_theme = dict()
+    for numero in range(len(liste_finale)):
+        print(liste_theme[numero])
+        print(index_theme)
+        if liste_theme[numero] == index_theme:
+            liste_dico.append(liste_finale[numero])
+    dico_theme[liste_titres[index_theme-1]] = liste_dico
+    return dico_theme
+
+creer_dico_theme(1)

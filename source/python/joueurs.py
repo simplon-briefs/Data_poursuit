20c44bcaaf5ba638c42501e8a77bcf17924e4764
CLOATRE-Erwan <erwan.cloatre@isen-ouest.yncrea.fr>
2020-12-01 12:18:58 +0100

    nouveau

--- source/python/joueurs.py
+++ source/python/joueurs.py
@@ -1,17 +1,17 @@
 
 class Joueur:
 
-    def __init__(self, id, nom, score, couleur, camembert):
+    def __init__(self, id, nom, couleur, score, camembert):
         self.id = id
         self.nom = nom
-        self.score = score
         self.couleur = couleur
+        self.score = score
         self.camembert = camembert
-
+        
     @classmethod
     def infos(cls, nb_joueurs):
         lst = []
         for i in range(nb_joueurs):
-            nouveau = Joueur(id, input(f"pseudo joueur n°{i+1}: "), 0, input("Choix de la couleur : "),0)
+            nouveau = Joueur(id, input(f"pseudo joueur n°{i+1}: "), input("Choix de la couleur : "), 0 ,0)
             lst.append(nouveau)
         return lst
